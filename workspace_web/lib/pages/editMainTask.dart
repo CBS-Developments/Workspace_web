b2de86c37209e7616e0244516bb97a1a3d8857c4
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2023-12-03 01:15:02 +0530

    Create editMainTask.dart

---
+++ workspace_web/lib/pages/editMainTask.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import '../componants.dart';
+
+class EditMainTaskPage extends StatefulWidget {
+  final MainTask mainTaskDetails;
+  const EditMainTaskPage({super.key, required this.mainTaskDetails});
+
+  @override
+  State<EditMainTaskPage> createState() => _EditMainTaskPageState();
+}
+
+class _EditMainTaskPageState extends State<EditMainTaskPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
