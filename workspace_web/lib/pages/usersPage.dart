80942cedfdc88570dcaa2b9cc166ca336ba15791
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2023-12-19 12:14:50 +0530

    Update usersPage.dart

--- workspace_web/lib/pages/usersPage.dart
+++ workspace_web/lib/pages/usersPage.dart
@@ -5,6 +5,7 @@ import 'dart:convert';
 import '../colors.dart';
 import '../componants.dart';
 
+
 class UsersPage extends StatefulWidget {
   const UsersPage({Key? key}) : super(key: key);
 
@@ -14,6 +15,8 @@ class UsersPage extends StatefulWidget {
 
 class _UsersPageState extends State<UsersPage> {
   List<User> userList = [];
+  int userCount = 0;
+  User? selectedUser;
 
   @override
   void initState() {
@@ -35,41 +38,196 @@ class _UsersPageState extends State<UsersPage> {
             fontSize: 20,
           ),
         ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: ElevatedButton(
+              onPressed: () {
+                // Navigator.pushNamed(context, '/createMainTask');
+              },
+              child: Text('Create User'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: AppColor.appDarkBlue,
+              ),
+            ),
+          ),
+          Center(child: Text(' $userCount    ', style: TextStyle(fontSize: 16))),
+        ],
       ),
       drawer: MyDrawer(),
       body: Row(
         children: [
-
           Expanded(
-            flex:1,
+            flex: 1,
             child: ListView.builder(
               itemCount: userList.length,
               itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(
-                    '${userList[index].firstName} ${userList[index].lastName}',
-                  ),
-                  subtitle: Text(userList[index].email),
-                  trailing: Switch(
-                    value: userList[index].activate == '1',
-                    onChanged: (value) {
-                      setState(() {
-                        userList[index].activate = value ? '1' : '0';
-                      });
-                      userStatusToggle(userList[index].userName, value);
-                    },
-                    activeColor: Colors.green,
-                    inactiveTrackColor: Colors.redAccent.withOpacity(0.5),
-                  ),
+                return Column(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(2.0),
+                      margin: const EdgeInsets.all(5.0),
+                      color: AppColor.appGrey,
+                      child: ListTile(
+                        title: Text(
+                          '${userList[index].firstName} ${userList[index].lastName}  |  ${userList[index].userName}',
+                        ),
+                        subtitle: Text(userList[index].email),
+                        trailing: Switch(
+                          value: userList[index].activate == '1',
+                          onChanged: (value) {
+                            setState(() {
+                              userList[index].activate = value ? '1' : '0';
+                            });
+                            userStatusToggle(userList[index].userName, value);
+                          },
+                          activeColor: Colors.green,
+                          inactiveTrackColor: Colors.redAccent.withOpacity(0.5),
+                        ),
+                        onTap: () {
+                          setState(() {
+                            selectedUser = userList[index];
+                          });
+                        },
+                      ),
+                    ),
+                    Divider()
+                  ],
                 );
               },
             ),
           ),
-          Expanded(flex:1,child: Column()),
+          Expanded(
+            flex: 1,
+            child: selectedUser != null ? userDetails(selectedUser!) : SizedBox(),
+          ),
         ],
       ),
     );
   }
+
+  Widget userDetails(User user) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 40),
+      margin: EdgeInsets.symmetric(horizontal: 40),
+      width: 200,
+      height: 300,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.white,
+        border: Border.all(
+          color: Colors.grey.shade500,
+          width: 1.0,
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'User Details:',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 18,
+            ),
+          ),
+          SizedBox(height: 10),
+          Text('ID: ${user.employeeID}',
+            style: TextStyle(
+              fontSize: 16,
+              color: Colors.black87,
+            ),
+          ),
+          SizedBox(height: 10),
+          Text('${user.firstName} ${user.lastName}',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.black87,
+            ),
+          ),
+          Text(
+            user.designation,
+            style: TextStyle(
+              fontSize: 16,
+              color: Colors.grey[700],
+            ),
+          ),
+          SizedBox(height: 20),
+          Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 2.0),
+                child: Icon(Icons.phone,size: 16,),
+              ),
+              Text(
+                'Tel: ',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black87,
+                ),
+              ),
+              Text(
+                '0${user.phone}',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black87,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 5),
+          Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 3.0),
+                child: Icon(Icons.email_outlined,size: 16,),
+              ),
+              Text(
+                'Email: ',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black87,
+                ),
+              ),
+              Text(
+                '${user.email}',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black87,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 5),
+          Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 3.0),
+                child: Icon(Icons.maps_home_work_outlined,size: 16,),
+              ),
+              Text(
+                'Company: ',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black87,
+                ),
+              ),
+              Text(
+                user.company,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black87,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 50),
+        ],
+      ),
+    );
+  }
+
 
   Future<void> getUserList() async {
     userList.clear();
@@ -93,6 +251,7 @@ class _UsersPageState extends State<UsersPage> {
         }
       });
       print("User Count: ${userList.length}");
+      userCount = userList.length;
     } else {
       throw Exception('Failed to load users from API');
     }
